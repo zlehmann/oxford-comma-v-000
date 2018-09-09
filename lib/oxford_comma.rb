708ff9649d2d88b382003d4cfb87f5f4cff0ccb9
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 15:48:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
   length = array.length
   case length
   when 1
-    puts "#{array[0]}"
+    return "#{array[0]}"
   when 2
-    puts "#{array[0]} and #{array[1]}"
+    return "#{array[0]} and #{array[1]}"
   else
     oxford = "and #{array.last}"
     array[-1] = oxford
