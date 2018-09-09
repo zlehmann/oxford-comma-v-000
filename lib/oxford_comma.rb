b1c023d692c59133f0ff018e79515d64235036aa
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 15:43:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,16 @@
 require 'pry'
 
 def oxford_comma(array)
-  oxford = "and #{array.last}"
-  array[-1] = oxford
-  puts "#{array.join(", ")}"
-  binding.pry
+  length = array.length
+  case length
+  when 1
+    puts "#{array[0]}"
+  when 2
+    puts "#{array[0]} and #{array[1]}"
+  else
+    oxford = "and #{array.last}"
+    array[-1] = oxford
+    puts "#{array.join(", ")}"
+  end
+  #binding.pry
 end
