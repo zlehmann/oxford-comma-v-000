61990b830f8fa91c245101b6fbfa4756cf87f20e
zlehmann@gmail.com <zlehmann@gmail.com>
2018-09-09 15:49:24 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+def oxford_comma(array)
+  length = array.length
+  case length
+  when 1
+    return "#{array[0]}"
+  when 2
+    return "#{array[0]} and #{array[1]}"
+  else
+    oxford = "and #{array.last}"
+    array[-1] = oxford
+    return "#{array.join(", ")}"
+  end
+  #binding.pry
 end
